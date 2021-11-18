5240466d356b6a2d39370871f645ff08a732f4ee
Enrique Chamaya <echamaya.ti@gmail.com>
2021-11-18 18:30:32 -0500

    correccion look and feel splash

--- src/main/java/com/encosoft/vista/SplashLog.java
+++ src/main/java/com/encosoft/vista/SplashLog.java
@@ -103,6 +103,22 @@ public class SplashLog extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
+                try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
         SplashLog splash = new SplashLog(); //Inicializar formulario splash
         splash.setVisible(true); 
         try{
